cc09d40ee245ea8815256defb2dac71441dc221c
Yevhenii Baibikov <jeka.spets333@gmail.com>
2025-10-04 20:14:25 +0200

    added api urls and registered models in admin

--- backend/theater/admin.py
+++ backend/theater/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import User, Spectacle, Ticket, Partner
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Spectacle)
+admin.site.register(Ticket)
+admin.site.register(Partner)
+
--- backend/theater_tresk/urls.py
+++ backend/theater_tresk/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from theater.views import TestView
 
 urlpatterns = [
     path('', TestView.as_view(), name='index'),
     path('admin/', admin.site.urls),
+    path('api/', include('theater.urls')),
 ]
